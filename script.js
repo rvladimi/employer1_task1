de6ef268c1a659959ad9663cd15865cb6ec7e473
rushay <vladimir.rushay@yandex.ru>
2025-05-05 18:27:47 +0300

    improved adaptive 2

--- script.js
+++ script.js
@@ -16,35 +16,4 @@ document.addEventListener("DOMContentLoaded", () => {
       header.classList.add("white");
     });
   }
-
-  // при уменьшении ширины окна до определенного предела
-  // верхняя и средняя правая картинки переносятся вниз
-  const upper = document.querySelector(".upper");
-  const upperLeft = document.querySelector(".upper-left");
-  const upperPicture = document.querySelector(".upper-picture");
-  const middle = document.querySelector(".middle");
-  window.addEventListener("resize", () => {
-    //console.log(document.documentElement.clientWidth - headerWidth);
-    if (upper) {
-      if (document.documentElement.clientWidth < headerWidth + 700) {
-        upper.style.flexDirection = "column";
-        upperLeft.style.width = "100%";
-        header.style.width = "100%";
-        upperPicture.style.marginLeft = "auto";
-        upperPicture.style.marginRight = "auto";
-        upperPicture.style.marginTop = "20px";
-        middle.style.flexWrap = "wrap";
-        middle.style.gap = "20px";
-      } else {
-        upper.style.flexDirection = "row";
-        upperLeft.style.width = "63%";
-        header.style.width = "50%";
-        upperPicture.style.marginLeft = "0px";
-        upperPicture.style.marginRight = "0px";
-        upperPicture.style.marginTop = "0px";
-        middle.style.flexWrap = "nowrap";
-        middle.style.gap = "0px";
-      }
-    }
-  });
 });
